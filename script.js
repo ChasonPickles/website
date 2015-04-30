ab952a22f9a73d4f5a69860db49b7d6cfc2caa56
ChasonPickles <harshbindra@Harshs-MacBook-Pro.local>
2015-04-29 23:31:03 -0400

    1st commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*jslint undef: true */
+
+$(document).ready(main);
+
+var main = function () {
+    /*
+    $('.about-me').click(function(){
+    move screen to about-me section
+    })
+*/
+    "use strict";
+    $(".row").click(function () {
+        $(this).hide();
+    });
+};
+
